c1c70bf6c5df610428eeb1d0648445dc8f29b94c
dirkarnez <smalldirkalex@gmail.com>
2024-12-09 17:26:41 +0800

    - wip

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 [whisper-loopback-transcription](https://github.com/dirkarnez/whisper-loopback-transcription)
 =============================================================================================
 Based on [tez3998/loopback-capture-sample: Sample code which records system (speaker) sounds (what you hear) in Python.](https://github.com/tez3998/loopback-capture-sample)
+
+### Mono (`data=data[:, 0]`)
+```python
+    # record audio with loopback from default speaker.
+    data = mic.record(numframes=SAMPLE_RATE*RECORD_SEC)
+    
+    # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
+    sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
+```
+
+### Stereo (`data=data`)
+```python
+    # record audio with loopback from default speaker.
+    data = mic.record(numframes=SAMPLE_RATE*RECORD_SEC)
+    
+    # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
+    sf.write(file=OUTPUT_FILE_NAME, data=data, samplerate=SAMPLE_RATE)
+```
+
+### Stereo Stream
+```python
+    while True:
+        data = mic.record()
+        print(data.size)
+```
--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-import soundcard as sc
-import soundfile as sf
+# import soundcard as sc
+# import soundfile as sf
 
-OUTPUT_FILE_NAME = "out.wav"    # file name.
-SAMPLE_RATE = 48000              # [Hz]. sampling rate.
-RECORD_SEC = 5                  # [sec]. duration recording audio.
+# OUTPUT_FILE_NAME = "out.wav"    # file name.
+# SAMPLE_RATE = 48000              # [Hz]. sampling rate.
+# RECORD_SEC = 5                  # [sec]. duration recording audio.
 
-with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
-    # record audio with loopback from default speaker.
-    data = mic.record(numframes=SAMPLE_RATE*RECORD_SEC)
+# with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
+#     # record audio with loopback from default speaker.
+#     while True:
+#         data = mic.record()
+#         print(data.size)
     
-    # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
-    sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
+#     # # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
+#     # sf.write(file=OUTPUT_FILE_NAME, data=data, samplerate=SAMPLE_RATE)
+import sys
+import random
+from PySide6 import QtCore, QtWidgets
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint | QtCore.Qt.WindowType.WindowStaysOnTopHint)
+    widget.show()
+
+    sys.exit(app.exec())
